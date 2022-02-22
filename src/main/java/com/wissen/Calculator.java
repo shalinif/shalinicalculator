9b1bc9229fc7661e43de729647046ca172d62c98
shalinif <shalini.farkya@wissen.com>
2022-02-22 14:13:01 +0530

    added file

---
+++ src/main/java/com/wissen/Calculator.java
@@ -0,0 +1,21 @@
+package com.wissen;
+
+public class Calculator {
+
+	public static int add(int a, int b)
+	{
+		return a+b;
+	}
+	
+	public static int sub(int a, int b)
+	{
+		return a-b;
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println(add(2,3));
+		System.out.println(sub(6,2));
+	}
+}
+
